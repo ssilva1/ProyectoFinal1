70a04f91f86075e9f23c2fb403f9e28efcdb40be
Lucas Lopez <lucas.lopez@uala.com.ar>
2022-09-03 18:28:05 -0300

    Add urls file

--- EntregaFinal1/Entregafinal1/urls.py
+++ EntregaFinal1/Entregafinal1/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppLavatres import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+""""
+urlpatterns = [
+    path('AppLavatres/', include('AppLavatres.urls')),
+]
+"""
+
+path('',views.inicio),
+path('vehiculos',views.vehiculos),
+path('indumentarias',views.indumentarias),
+path('animales',views.animales),
+
+
+
+
+
