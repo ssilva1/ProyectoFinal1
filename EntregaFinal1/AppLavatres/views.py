d1aa8a5c540693f828aabbae2f65a16ba75c86d6
Lucas Lopez <lucas.lopez@uala.com.ar>
2022-09-03 17:55:51 -0300

    Commit ViewsFile

--- EntregaFinal1/AppLavatres/views.py
+++ EntregaFinal1/AppLavatres/views.py
@@ -1,3 +1,27 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request)
+
+    return HttpResponse('Vista inicio')
+
+
+def vehiculo(request)
+
+    return HttpResponse('Vista Vehiculo')
+
+def indumentaria(request)
+
+    return HttpResponse('Vista Indumentaria')
+
+def animal(request)
+
+    return HttpResponse('Vista Animal')
+
+    
+
+
+
+
+
