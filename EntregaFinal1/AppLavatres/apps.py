3ddf4631c2ecd7d1fbae801b77874fb6337d186f
ssilva1 <silvasergio@live.com.ar>
2022-09-03 19:30:01 -0300

    SE corrige error en apps.py

--- EntregaFinal1/AppLavatres/apps.py
+++ EntregaFinal1/AppLavatres/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LavatresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lavatres'
+    name = 'AppLavatres'
--- EntregaFinal1/AppLavatres/models.py
+++ EntregaFinal1/AppLavatres/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
-from django.db import models
-
-class Vehiculo (models.Model):
+class Vehiculo(models.Model):
     tipo_vehiculo = models.CharField(max_length=30)
     aspirado = models.BooleanField()
     dominio = models.CharField(max_length=7)
     ingreso = models.DateTimeField()
     egreso = models.DateTimeField()
     
-class Indumentaria (models.Model):
+class Indumentaria(models.Model):
     tipo_indumentaria = models.CharField(max_length=40)
     ropa_banca = models.BooleanField()
     nombre_cliente = models.CharField(max_length=30)
     fecha_retiro = models.DateField()
     
-class Animal (models.Model):
+class Animal(models.Model):
     tipo_animal = models.CharField(max_length=30)
     nombre_duenio = models.CharField(max_length=30)
     corte_pelo = models.BooleanField()
