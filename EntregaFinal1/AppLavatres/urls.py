93db710cea0b1c58b4cbb38d997638e0739537b2
Matias Acuna <m.matiasariel@gmail.com>
2022-09-04 20:42:31 -0300

    Se editaron formularios, templates, urls y vistas

--- EntregaFinal1/AppLavatres/urls.py
+++ EntregaFinal1/AppLavatres/urls.py
@@ -6,4 +6,6 @@ from AppLavatres.views import *
 urlpatterns = [
     path('', inicio, name='AppLavatresInicio'),
     path('carga_vehiculo/', vehiculo_formulario, name='AppLavatresAutoFormulario'),
+    path('carga_indumentaria/', indumentaria_formulario, name='AppLavatresIndumentariaFormulario'),
+    path('carga_animal/', animales_formulario, name='AppLavatresAnimalesFormulario'),
     ]
