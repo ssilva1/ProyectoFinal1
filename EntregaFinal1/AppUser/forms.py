6ecedc7106c58bffc7a2a370eb347aca70c0dbc9
Joel Peralta <joelsk8_1@hotmail.com>
2022-09-30 20:09:42 -0300

    se agregaron carga y busqueda post login

--- EntregaFinal1/AppUser/forms.py
+++ EntregaFinal1/AppUser/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , PasswordChangeForm
 from django.contrib.auth.models import User
 
 from AppUser.models import Avatar
@@ -10,9 +10,11 @@ from AppUser.models import Avatar
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     telefono = forms.IntegerField(required=False)
+    password1 = forms.CharField()
+    password2 = forms.CharField()
     class Meta:
         model = User
-        fields = ('username', 'email', 'telefono')
+        fields = ('username', 'email', 'telefono','password1', 'password2')
         
         
         
@@ -21,4 +23,16 @@ class AvatarForm(forms.ModelForm):
     class Meta:
         model = Avatar
         fields = "__all__"
+        
+        
+        
+class UserEditForm(UserCreationForm):
+    email = forms.EmailField()
+    telefono = forms.IntegerField(required=False)
+    # password = forms.CharField()
+    # password2 = forms.CharField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'telefono',]
+        help_texts = {k: "" for k in fields}
         
